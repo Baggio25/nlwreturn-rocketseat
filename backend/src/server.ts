0961dcac4d8cc8e3a8622f5047cb52a54cdcb618
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-05-04 21:36:56 -0300

    Send e-mail

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express, { application } from "express";
-
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "27ba3ae6b6e197",
+    pass: "7b533e1130341d",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -17,7 +26,18 @@ app.post("/feedbacks", async (req, res) => {
     },
   });
 
-  return res.status(201).json({data: feedback});
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Rodrigo Baggio <rodrigo.baggio.si@gmail.com>",
+    subject: "Novo Feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #222;">`,
+      `<p>Tipo: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
