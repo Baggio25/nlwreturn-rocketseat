6045dc8710d9c71d9335d4e6666eb1259c8fd00b
rodrigo.baggio.si@gmail.com <Baggio25>
2022-05-05 14:03:45 -0300

    Fonts

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from "expo-status-bar";
+import React from 'react';
 import { View } from "react-native";
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from "expo-status-bar";
+
+import { 
+  Inter_400Regular, 
+  Inter_500Medium, 
+  useFonts 
+} from '@expo-google-fonts/inter';
 
 import { Widget } from "./src/components/Widget";
 import { theme } from "./src/theme";
 
-
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View
       style={{
